20d326d865f8a4c0b97e81162e0de767fc74e6e6
Flytre <flytre7@gmail.com>
2022-05-04 11:08:33 -0400

    -Ported to forge! -1.18.2

---
+++ Common/src/main/java/net/flytre/dual_riders/Constants.java
@@ -0,0 +1,14 @@
+package net.flytre.dual_riders;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Constants {
+    public static final String MOD_ID = "dual_riders";
+    public static final String MOD_NAME = "Dual Riders";
+    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+
+
+    public static void main(String[] args) {
+    }
+}
---
+++ Forge/src/main/java/net/flytre/dual_riders/ForgeMod.java
@@ -0,0 +1,12 @@
+package net.flytre.dual_riders;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+@Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ForgeMod {
+
+    public ForgeMod() {
+    }
+}
